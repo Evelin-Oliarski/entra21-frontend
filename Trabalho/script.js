7016f1166abc28f350820d6022f88f8218facf6c
Evelin Paola Macedo Oliarski <evelin.pm.oliarski@gmail.com>
2026-05-29 21:50:46 -0300

    Alterações na pasta Trabalho

--- Trabalho/script.js
+++ Trabalho/script.js
@@ -1,9 +1,15 @@
 const form = document.getElementById('form');
+const tamanhoTexto = document.getElementById('tamanhoTexto');
+const spanErro = document.getElementById('mensagemErro');
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
-    buscarFato();
+    const numeroValido = validarNumeroTexto();
+
+    if(numeroValido){
+        buscarFato();
+    }
 });
 
 async function buscarFato() {
@@ -21,3 +27,27 @@ async function buscarFato() {
         console.error('Falha na comunicação!', error);
     }
 }
+
+function validarNumeroTexto(){
+    if(tamanhoTexto.value < 20){
+        tamanhoTexto.classList.add('borda-vermelha');
+        spanErro.textContent = 'O texto tem que ter no mínimo 20 espaçamentos.';
+        return false;
+    } else if(tamanhoTexto.value > 250){
+        tamanhoTexto.classList.remove('borda-vermelha');
+        spanErro.textContent = 'O texto tem que ter no máximo 250 espaçamentos.';
+        return false;
+    } else {
+        tamanhoTexto.classList.remove('borda-vermelha');
+        spanErro.textContent = '';
+        return true;
+    }
+}
+
+tamanhoTexto.addEventListener('input', function(){
+    if(tamanhoTexto !== ''){
+        tamanhoTexto.classList.remove('borda-vermelha');
+        spanErro.textContent = '';
+    }
+});
+
